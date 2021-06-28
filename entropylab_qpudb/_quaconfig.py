34971fe84dd3a522a9018d903ef8b7f44d793775
Lior Ella <lior@quantum-machines.co>
2021-06-28 06:41:06 +0300

    bugfix in qua config

--- entropylab_qpudb/_quaconfig.py
+++ entropylab_qpudb/_quaconfig.py
@@ -132,12 +132,12 @@ class QuaConfig(_UserDict):
             mixer = self.data["elements"][element]["mixInputs"]["mixer"]
             lo_freq = self.data["elements"][element]["mixInputs"]["lo_frequency"]
             found_entry_index = None
-            for entry in self.data["mixers"][mixer]:
+            for i, entry in enumerate(self.data["mixers"][mixer]):
                 if (
                     entry["intermediate_frequency"] == old_if
                     and entry["lo_frequency"] == lo_freq
                 ):
-                    found_entry_index = entry
+                    found_entry_index = i
             self.data["mixers"][mixer][found_entry_index][
                 "intermediate_frequency"
             ] = new_if
